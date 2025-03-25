d07cc32703f7210e1282de62dafb5142f3e9c379
henriquejosefreitas <henrique.j.moreira@aluno.senai.br>
2025-03-24 21:41:56 -0300

    Props e estilização com condicional

--- segundo/src/App.jsx
+++ segundo/src/App.jsx
@@ -2,25 +2,57 @@ import { useState } from 'react'
 import './App.css'
 import ShowUserName from './components/ShowUserName'
 import Time from './components/Time'
-import Spotify from './components/Spotify'
+import Singers from './components/Singers'
 
 function App() {
-  const nome = 'Cris'
-  const [name] = useState("Ney")
+  const titulo = true;
+  const nj = 10;
+  const nome = 'Cris';
+  const [name] = useState("Ney");
+  const [cantores] = useState([
+    {id:1, nome:"Taylor Swift", estiloMusical:"Pop", premiado:true},
+    {id:2, nome:"Michael Jackson", estiloMusical:"R&B", premiado:true},
+    {id:3, nome:"Matuê", estiloMusical:"Trap", premiado:false},
+  ])
   return (
     <>
+    {/* ShowUserName */}
       <ShowUserName name={nome} shirt="7" />
       <hr />
+    {/* Time */}
       <Time nome="São Paulo" estado="SP" posicao={1} campeao={true}/>
       <Time nome="Real Madrid" estado="MD" posicao={2} campeao={true}/>
       <Time nome="Vasco Da Gama" estado="RJ" posicao={100}campeao={false}/>
       <hr />
-      <Spotify id={1} nome="Beyoncé" estiloMusical="Hip-Hop" premiado={true}/>
-      <Spotify id={2} nome="Katy Perry" estiloMusical="Rock" premiado={false}/>
-      <Spotify id={3} nome="Kendrick Lamar" estiloMusical="Rap" premiado={true}/>
-      <Spotify id={4} nome="Post Malone" estiloMusical="Country" premiado={false}/>
+    {/* Singers */}
+      {
+        cantores.map((cantor) => (
+         <Singers 
+         key={cantor.id} 
+         nome={cantor.nome} 
+         estiloMusical={cantor.estiloMusical} 
+         premiado={cantor.premiado}/> 
+        ))
+      }
+      <hr />
+      <h2 style={ nj > 5 
+      ? {color:"green", backgroundColor:"yellow", fontSize:"30px"} 
+      : {color:"red", backgroundColor:"blue", fontSize:"30px"}
+      }>
+        ESTE CSS É DINâMICO
+      </h2>
+      <h2 style={ nj < 5 
+      ? {color:"green", backgroundColor:"yellow", fontSize:"30px"} 
+      : {color:"red", backgroundColor:"blue", fontSize:"30px"}
+      }>
+        ESTE CSS TAMBEM É DINâMICO
+      </h2>
+
+      <h1
+      className={titulo ? "titulo-incrivel" : "texto-paia"}
+      >Titulo incrivel pra teste</h1>
     </>
-  )
+  );
 }
 
 export default App
